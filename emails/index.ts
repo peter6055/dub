666bdbfe35e156cd7f34c73bd9b3893cb07227b6
peter6055 <59694246+peter6055@users.noreply.github.com>
2023-07-05 15:27:14 +1000

    email change

--- emails/index.ts
+++ emails/index.ts
@@ -1,16 +1,46 @@
 import { buildSendMail } from "mailing-core";
 import nodemailer from "nodemailer";
+//
+// const sendMail = buildSendMail({
+//   transport: nodemailer.createTransport({
+//     host: process.env.POSTMARK_API_KEY,
+//     port: 587,
+//     auth: {
+//       user: process.env.POSTMARK_API_KEY,
+//       pass: process.env.POSTMARK_API_KEY,
+//     },
+//   }),
+//   defaultFrom: "Steven from Dub <steven@internal-short.shopmy.com.au>",
+//   configPath: "./mailing.config.json",
+// });
+//
+// export default sendMail;
+//
+// export const sendMarketingMail = buildSendMail({
+//   transport: nodemailer.createTransport({
+//     host: "smtp-broadcasts.postmarkapp.com",
+//     port: 587,
+//     auth: {
+//       user: process.env.POSTMARK_MARKETING_API_KEY,
+//       pass: process.env.POSTMARK_MARKETING_API_SECRET,
+//     },
+//   }),
+//   defaultFrom: "Steven from Dub <steven@ship.internal-short.shopmy.com.au>",
+//   configPath: "./mailing.config.json",
+// });
 
+
+// SendGrid
 const sendMail = buildSendMail({
   transport: nodemailer.createTransport({
-    host: "smtp.postmarkapp.com",
+    host: "smtp.sendgrid.net",
     port: 587,
     auth: {
-      user: process.env.POSTMARK_API_KEY,
-      pass: process.env.POSTMARK_API_KEY,
+      user: process.env.EMAIL_SMTP_USERNAME,
+      pass: process.env.EMAIL_SMTP_PASSWORD
     },
   }),
-  defaultFrom: "Steven from Dub <steven@internal-short.shopmy.com.au>",
+  defaultFrom: "SHOPMY 员工 <emp_no_reply@shopmy.com.auu>",
   configPath: "./mailing.config.json",
 });
 
@@ -18,13 +48,13 @@ export default sendMail;
 
 export const sendMarketingMail = buildSendMail({
   transport: nodemailer.createTransport({
-    host: "smtp-broadcasts.postmarkapp.com",
+    host: "smtp.sendgrid.net",
     port: 587,
     auth: {
-      user: process.env.POSTMARK_MARKETING_API_KEY,
-      pass: process.env.POSTMARK_MARKETING_API_SECRET,
+      user: process.env.EMAIL_SMTP_USERNAME,
+      pass: process.env.EMAIL_SMTP_PASSWORD
     },
   }),
-  defaultFrom: "Steven from Dub <steven@ship.internal-short.shopmy.com.au>",
+  defaultFrom: "SHOPMY 员工 <emp_no_reply@shopmy.com.auu>",
   configPath: "./mailing.config.json",
 });
