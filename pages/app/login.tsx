fb94986856affda001a0593b7f20e1b41162d832
peter6055 <59694246+peter6055@users.noreply.github.com>
2023-07-05 19:22:09 +1000

    remove email login and sign up

--- pages/app/login.tsx
+++ pages/app/login.tsx
@@ -51,7 +51,7 @@ export default function Login() {
         </div>
         <div className="flex flex-col space-y-3 bg-gray-50 px-4 py-8 sm:px-16">
           <Button
-            text="Continue with Google"
+            text="Continue with Google SHOPMY EAS"
             onClick={() => {
               setClickedGoogle(true);
               signIn("google", {
@@ -62,104 +62,104 @@ export default function Login() {
             disabled={clickedEmail}
             icon={<Google className="h-4 w-4" />}
           />
-          <form
-            onSubmit={async (e) => {
-              e.preventDefault();
-              setClickedEmail(true);
-              fetch("/api/auth/account-exists", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ email }),
-              })
-                .then(async (res) => {
-                  const { exists } = await res.json();
-                  if (exists) {
-                    signIn("email", {
-                      email,
-                      redirect: false,
-                      ...(next && next.length > 0 ? { callbackUrl: next } : {}),
-                    }).then((res) => {
-                      setClickedEmail(false);
-                      if (res?.ok && !res?.error) {
-                        setEmail("");
-                        toast.success("Email sent - check your inbox!");
-                      } else {
-                        toast.error("Error sending email - try again?");
-                      }
-                    });
-                  } else {
-                    toast.error("No account found with that email address.");
-                    setNoSuchAccount(true);
-                    setClickedEmail(false);
-                  }
-                })
-                .catch(() => {
-                  setClickedEmail(false);
-                  toast.error("Error sending email - try again?");
-                });
-            }}
-            className="flex flex-col space-y-3"
-          >
-            {showEmailOption && (
-              <div>
-                <div className="mb-4 mt-1 border-t border-gray-300" />
-                <input
-                  id="email"
-                  name="email"
-                  autoFocus
-                  type="email"
-                  placeholder="panic@thedis.co"
-                  autoComplete="email"
-                  required
-                  value={email}
-                  onChange={(e) => {
-                    setNoSuchAccount(false);
-                    setEmail(e.target.value);
-                  }}
-                  className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-                />
-              </div>
-            )}
-            <Button
-              text="Continue with Email"
-              variant="secondary"
-              {...(!showEmailOption && {
-                type: "button",
-                onClick: (e) => {
-                  e.preventDefault();
-                  setShowEmailOption(true);
-                },
-              })}
-              loading={clickedEmail}
-              disabled={clickedGoogle}
-            />
-          </form>
-          <Button
-            text="Continue with SAML SSO"
-            disabled
-            disabledTooltip={<SSOWaitlist />}
-          />
+          {/*<form*/}
+          {/*  onSubmit={async (e) => {*/}
+          {/*    e.preventDefault();*/}
+          {/*    setClickedEmail(true);*/}
+          {/*    fetch("/api/auth/account-exists", {*/}
+          {/*      method: "POST",*/}
+          {/*      headers: { "Content-Type": "application/json" },*/}
+          {/*      body: JSON.stringify({ email }),*/}
+          {/*    })*/}
+          {/*      .then(async (res) => {*/}
+          {/*        const { exists } = await res.json();*/}
+          {/*        if (exists) {*/}
+          {/*          signIn("email", {*/}
+          {/*            email,*/}
+          {/*            redirect: false,*/}
+          {/*            ...(next && next.length > 0 ? { callbackUrl: next } : {}),*/}
+          {/*          }).then((res) => {*/}
+          {/*            setClickedEmail(false);*/}
+          {/*            if (res?.ok && !res?.error) {*/}
+          {/*              setEmail("");*/}
+          {/*              toast.success("Email sent - check your inbox!");*/}
+          {/*            } else {*/}
+          {/*              toast.error("Error sending email - try again?");*/}
+          {/*            }*/}
+          {/*          });*/}
+          {/*        } else {*/}
+          {/*          toast.error("No account found with that email address.");*/}
+          {/*          setNoSuchAccount(true);*/}
+          {/*          setClickedEmail(false);*/}
+          {/*        }*/}
+          {/*      })*/}
+          {/*      .catch(() => {*/}
+          {/*        setClickedEmail(false);*/}
+          {/*        toast.error("Error sending email - try again?");*/}
+          {/*      });*/}
+          {/*  }}*/}
+          {/*  className="flex flex-col space-y-3"*/}
+          {/*>*/}
+          {/*  {showEmailOption && (*/}
+          {/*    <div>*/}
+          {/*      <div className="mb-4 mt-1 border-t border-gray-300" />*/}
+          {/*      <input*/}
+          {/*        id="email"*/}
+          {/*        name="email"*/}
+          {/*        autoFocus*/}
+          {/*        type="email"*/}
+          {/*        placeholder="panic@thedis.co"*/}
+          {/*        autoComplete="email"*/}
+          {/*        required*/}
+          {/*        value={email}*/}
+          {/*        onChange={(e) => {*/}
+          {/*          setNoSuchAccount(false);*/}
+          {/*          setEmail(e.target.value);*/}
+          {/*        }}*/}
+          {/*        className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"*/}
+          {/*      />*/}
+          {/*    </div>*/}
+          {/*  )}*/}
+          {/*  <Button*/}
+          {/*    text="Continue with Email"*/}
+          {/*    variant="secondary"*/}
+          {/*    {...(!showEmailOption && {*/}
+          {/*      type: "button",*/}
+          {/*      onClick: (e) => {*/}
+          {/*        e.preventDefault();*/}
+          {/*        setShowEmailOption(true);*/}
+          {/*      },*/}
+          {/*    })}*/}
+          {/*    loading={clickedEmail}*/}
+          {/*    disabled={clickedGoogle}*/}
+          {/*  />*/}
+          {/*</form>*/}
+          {/*<Button*/}
+          {/*  text="Continue with SAML SSO"*/}
+          {/*  disabled*/}
+          {/*  disabledTooltip={<SSOWaitlist />}*/}
+          {/*/>*/}
 
-          {noSuchAccount ? (
-            <p className="text-center text-sm text-red-500">
-              No such account.{" "}
-              <Link href="/register" className="font-semibold text-red-600">
-                Sign up
-              </Link>{" "}
-              instead?
-            </p>
-          ) : (
-            <p className="text-center text-sm text-gray-500">
-              Don't have an account?{" "}
-              <Link
-                href="/register"
-                className="font-semibold text-gray-500 transition-colors hover:text-black"
-              >
-                Sign up
-              </Link>
-              .
-            </p>
-          )}
+          {/*{noSuchAccount ? (*/}
+          {/*  <p className="text-center text-sm text-red-500">*/}
+          {/*    No such account.{" "}*/}
+          {/*    <Link href="/register" className="font-semibold text-red-600">*/}
+          {/*      Sign up*/}
+          {/*    </Link>{" "}*/}
+          {/*    instead?*/}
+          {/*  </p>*/}
+          {/*) : (*/}
+          {/*  <p className="text-center text-sm text-gray-500">*/}
+          {/*    Don't have an account?{" "}*/}
+          {/*    <Link*/}
+          {/*      href="/register"*/}
+          {/*      className="font-semibold text-gray-500 transition-colors hover:text-black"*/}
+          {/*    >*/}
+          {/*      Sign up*/}
+          {/*    </Link>*/}
+          {/*    .*/}
+          {/*  </p>*/}
+          {/*)}*/}
         </div>
       </div>
     </div>
