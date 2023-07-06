28b98c57e102ec1c8c36d8f902bd915473582313
peter6055 <59694246+peter6055@users.noreply.github.com>
2023-07-06 22:23:23 +1000

    remove email login and sign up

--- components/app/links/link-card.tsx
+++ components/app/links/link-card.tsx
@@ -276,9 +276,17 @@ export default function LinkCard({ props }: { props: LinkProps }) {
             <h3 className="max-w-[200px] truncate text-sm font-medium text-gray-700 md:max-w-md xl:max-w-lg">
               {url}
             </h3>
-            <p>
-              {remark}
-            </p>
+            {
+                remark!== null &&
+                <>
+                  <h3 className="max-w-[200px] truncate text-sm font-medium text-gray-700 md:max-w-md xl:max-w-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                    </svg>
+                    {remark}
+                  </h3>
+                </>
+            }
           </div>
         </div>
 
